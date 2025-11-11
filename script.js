3bddc1919b6b0e7971075582a12dc2e19d713e95
SATHISH B <sathishb2310@gmail.com>
2025-11-11 12:50:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-// Complete the js code
-function Car(make, model) {}
+// Step 1: Create the Car constructor
+function Car(make, model) {
+    this.make = make; 
+    this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+Car.prototype.getMakeModel = function() {
+    return `${this.make} ${this.model}`; 
+};
+
+function SportsCar(make, model, topSpeed) {
+    Car.call(this, make, model); 
+    this.topSpeed = topSpeed; 
+}
+
+SportsCar.prototype.getTopSpeed = function() {
+    return this.topSpeed; 
+};
+
+SportsCar.prototype = Object.create(Car.prototype); 
+SportsCar.prototype.constructor = SportsCar; 
 
 // Do not change the code below
 window.Car = Car;
