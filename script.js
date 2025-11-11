0f67e75951e4ae736310f6b5c6f64e1e955ceda2
SATHISH B <sathishb2310@gmail.com>
2025-11-11 12:57:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 // Step 1: Create the Car constructor
 function Car(make, model) {
-    this.make = make; 
-    this.model = model;
+	Car(make,model){
+		this.make = make; 
+	    this.model = model;
+	}
+
+	getMakeModel(){
+		return this.make+" "+this.model;
+	}
 }
 
-Car.prototype.getMakeModel = function() {
-    return `${this.make} ${this.model}`; 
-};
+function SportsCar(make, model, topSpeed) extends Car
+{
+	SportsCar(){
+		super(this.make,this.model)
+		this.topSpeed = topSpeed; 
+	}
 
-function SportsCar(make, model, topSpeed) {
-    Car.call(this, make, model); 
-    this.topSpeed = topSpeed; 
-}
+	getTopSpeed(){
+		return this.topSpeed;
+	}
+} 
 
-SportsCar.prototype.getTopSpeed = function() {
-    return this.topSpeed; 
-};
-
-SportsCar.prototype = Object.create(Car.prototype); 
-SportsCar.prototype.constructor = SportsCar; 
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel());
+console.log(car.getTopSpeed()); 
 
 // Do not change the code below
 window.Car = Car;
